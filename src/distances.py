d514298b15568b97b55e26e76fdcdc383ef3710e
Vladislav-Kanev <vokanev@edu.hse.ru>
2023-12-03 18:10:34 +0300

    initial commit

---
+++ src/distances.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from scipy.spatial.distance import chebyshev, euclidean, cityblock, cosine
+
+
+def chebyshev_distances(data: pd.DataFrame):
+    distances = pd.DataFrame(index=range(len(data)), columns=range(len(data)))
+
+    for i in range(len(data)):
+        for j in range(i, len(data)):
+            distances.at[i, j] = distances.at[j, i] = chebyshev(
+                data.iloc[i], data.iloc[j])
+
+    return distances
+
+
+def euclidean_distances(data: pd.DataFrame):
+    distances = pd.DataFrame(index=range(len(data)), columns=range(len(data)))
+
+    for i in range(len(data)):
+        for j in range(i, len(data)):
+            distances.at[i, j] = distances.at[j, i] = euclidean(
+                data.iloc[i], data.iloc[j])
+
+    return distances
+
+
+def cosine_distances(data: pd.DataFrame):
+    distances = pd.DataFrame(index=range(len(data)), columns=range(len(data)))
+
+    for i in range(len(data)):
+        for j in range(i, len(data)):
+            distances.at[i, j] = distances.at[j, i] = cosine(
+                data.iloc[i], data.iloc[j])
+
+    return distances
+
+
+def convert_distance_to_similarity(distance_matrix: pd.DataFrame) -> pd.DataFrame:
+    return 1 / (1 + distance_matrix)
