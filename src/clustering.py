589ba8b5e980cc3d99b77047e49a1239fa5d09cf
Vladislav-Kanev <vokanev@edu.hse.ru>
2023-12-06 21:32:07 +0300

    fixed crushes

--- src/clustering.py
+++ src/clustering.py
@@ -1,10 +1,11 @@
+import numpy as np
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from scipy.spatial.distance import squareform
-from sklearn.cluster import KMeans, SpectralClustering
+from sklearn.cluster import KMeans, SpectralClustering, AgglomerativeClustering
 from sklearn.mixture import GaussianMixture
 
 
-def hierarchical_clustering(distance_matrix, clusters=2, method='single'):
+def hierarchical_clustering(data, distance_matrix, clusters=2, method='single'):
     """
     Perform hierarchical clustering with 2 clusters and plot the result.
 
@@ -16,11 +17,8 @@ def hierarchical_clustering(distance_matrix, clusters=2, method='single'):
     - The clustered DataFrame.
     """
 
-    condensed_distance = squareform(distance_matrix)
-
-    linkage_matrix = linkage(condensed_distance, method=method)
-
-    clusters = fcluster(linkage_matrix, clusters, criterion='maxclust')
+    clusters = AgglomerativeClustering(
+        n_clusters=clusters, linkage='single', connectivity=distance_matrix).fit_predict(data)
 
     return clusters
 
@@ -36,10 +34,13 @@ def kmeans_clustering(df, clusters=2):
     - The clustered DataFrame.
     """
     # Instantiate the KMeans model
-    kmeans = KMeans(n_clusters=clusters, n_init='auto', random_state=42)
+    kmeans = KMeans(n_clusters=clusters,  # ожидаемое число кластеров
+                    max_iter=300,  # максимальное число итераций K-means для одного запуска
+                    random_state=42,
+                    n_init="auto")
 
     # Fit the model to the data
-    clusters_list = kmeans.fit_predict(df)
+    clusters_list = kmeans.fit_predict(np.array(df))
 
     return clusters_list
 
@@ -57,15 +58,19 @@ def em_clustering(df, clusters=2):
     - The clustered DataFrame.
     """
     # Instantiate the em model
-    gmm = GaussianMixture(n_components=clusters, random_state=42)
+    gmm = GaussianMixture(n_components=clusters,
+                          covariance_type="full",  # тип используемых ковариационных параметров
+                          max_iter=300,
+                          init_params="random_from_data",
+                          random_state=42)
 
     # Fit the model to the data
-    clusters_list = gmm.fit_predict(df)
+    clusters_list = gmm.fit_predict(np.array(df))
 
     return clusters_list
 
 
-def spectral_clustering(df, clusters=2):
+def spectral_clustering(distance_matrix, clusters=2):
     """
     Perform Spectral Clustering clustering
 
@@ -78,10 +83,13 @@ def spectral_clustering(df, clusters=2):
     - The clustered DataFrame.
     """
     # Instantiate the sc model
-    sc = SpectralClustering(n_clusters=clusters,
-                            affinity='precomputed', random_state=42)
-
+    sc = SpectralClustering(n_components=clusters,  # ожидаемое число кластеров
+                            assign_labels='cluster_qr',  # стратегия назначения меток элементам в пространстве
+                            affinity="precomputed",  # интерпретировать ли входные данные как заранее вычисленную матрицу сходства, где большие значения указывают на большее сходство между экземплярами
+                            # случайное число для инициализации центроидов (centroid)
+                            random_state=42,
+                            )
     # Fit the model to the data
-    clusters_list = sc.fit_predict(df)
+    clusters_list = sc.fit_predict(np.array(distance_matrix))
 
     return clusters_list
